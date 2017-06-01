6eaf08b8c13ac0165d6c77ca598df2b6fae5fec2
Jean Freitas <henriquek3@live.com>
2017-05-31 22:32:55 -0400

    dev

--- index.php
+++ index.php
@@ -8,9 +8,11 @@ use FREITECH\Ecz\Ctrl;
 require 'App/views/header.html';
 require 'App/views/menus.html';
 
+
 /*
  * Implementar sistema de rotas para add conteudo das paginas aqui
  */
+
 
 Ctrl\IndexController::renderView(Ctrl\IndexController::getUrl());
 
